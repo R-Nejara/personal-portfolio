7cfc1f472d6c2c3ce4db2c0d4b9a45979bb85b9f
Rayan Nejara <rayannejara22u@gmail.com>
2026-01-20 04:39:00 +0100

    feat: add ambient circle to LandingPage.jsx and correct page spacing

--- src/components/landingpage/LandingPage.jsx
+++ src/components/landingpage/LandingPage.jsx
@@ -1,10 +1,16 @@
 import './LandingPage.css'
+import LeftSide from './landingpage_components/LeftSide'
 
 function LandingPage() {
 
   return (
     <>
-    <main className="landing-area"></main>
+      <main className="landing-main">
+        <div className="landing-area">
+          <LeftSide />
+          <div className="ambiente-circle"></div>
+        </div>
+      </main>
     </>
   )
 }
