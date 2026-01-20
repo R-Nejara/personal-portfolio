ddc4959178d6a038f191b412d6e95935a59bb9f6
Rayan Nejara <rayannejara22u@gmail.com>
2026-01-20 04:41:18 +0100

    feat: adding right site components for style preparations

---
+++ src/components/landingpage/landingpage_components/LeftSide.jsx
@@ -0,0 +1,29 @@
+function LeftSide() {
+
+  return (
+    <>
+      <h4 className="intruduction-title">web developer & ui/ux designer</h4>
+      <h1 className="name">rayan nejara</h1>
+      <h3 className="location">germany</h3>
+      <div className="tech-stack">
+        <ul className="skill-list">
+          <li className="skill skill1">Figma</li>
+          <li className="skill skill2">UI/UX Design</li>
+          <li className="skill skill3">JavaScript</li>
+          <li className="skill skill4">HTML</li>
+          <li className="skill skill5">CSS</li>
+        </ul>
+      </div>
+      <p>
+        Ich entwickle Webapps, um Ideen kreativ umzusetzen und fertige Projekte zu schaffen,
+        die meine Begeisterung für Technik widerspiegeln – von virtuellen Showrooms bis zu interaktiven Experimenten.
+      </p>
+      <div className="landing-buttons">
+      <div className="btn project-btn">projekte</div>
+      <div className="btn contact-btn">kontakt</div>
+      </div>
+    </>
+  )
+}
+
+export default LeftSide
